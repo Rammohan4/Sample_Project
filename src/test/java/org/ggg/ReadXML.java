3a327b96ae4a73502df49cd54a0b69d29c90c58c
Rammohan4 <crammohan55@gmail.com>
2024-07-03 09:39:23 +0530

    Project Structure

---
+++ src/test/java/org/ggg/ReadXML.java
@@ -0,0 +1,42 @@
+package org.ggg;
+
+import java.io.File;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+public class ReadXML {
+public static void main(String[] args) throws JAXBException {
+	xmlRead();
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void xmlRead() throws JAXBException {
+		JAXBContext jb = JAXBContext.newInstance(GGG.class);
+		Unmarshaller aaa = jb.createUnmarshaller();
+		File loc=new File ("C:\\Users\\ramrc\\Videos\\ABC\\src\\test\\resources\\xml\\samples.xml");
+		Object obj = aaa.unmarshal(loc);
+		GGG s=(GGG)obj;
+		
+		
+		System.out.println(s.getId());
+		System.out.println(s.getName());
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
